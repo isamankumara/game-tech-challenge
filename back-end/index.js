4a814358c6730f9539e7956360272e22910812d9
Saman <me@isamankumara.com>
2022-10-31 03:35:14 +0530

    add docker env

--- back-end/index.js
+++ back-end/index.js
@@ -98,7 +98,10 @@ io.on('connection', (socket) => {
         }
     });
 });
+app.get('/', (req, res) => {
+    res.send('<h1>Hello world</h1>');
+});
 
-server.listen(4000, () => {
-    console.log('listening on *:4000');
+server.listen(3000, () => {
+    console.log('listening on *:3000');
 });
